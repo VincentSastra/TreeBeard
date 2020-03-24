3497437315698c51e22230369675bf6e7c425e15
vincentsastra <vincent.sastra@gmail.com>
2020-03-24 05:07:26 -0700

    delete unused code in routes and comment

--- lib/screens/routes.dart
+++ lib/screens/routes.dart
@@ -3,36 +3,16 @@ import 'package:flutter/material.dart';
 import 'goal/screen_goal.dart';
 import 'home/screen_home.dart';
 
-Route toGoal(Offset start) {
-  return changePage(start, GoalPage());
-}
-
-Route toHome(Offset start) {
-  return changePage(start, HomePage());
-}
-
-Route changePage(Offset start, Widget widget) {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => widget,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = start;
-      var end = Offset.zero;
-      var curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
+/// A global variable of the threshold of the drag to change page
+int dragThreshold() {
+  return 10;
 }
 
 class SlideTransitionRoute<T> extends MaterialPageRoute<T> {
 
   Offset offset;
 
+  /// A page route that slides the end page from offset Offset
   SlideTransitionRoute({WidgetBuilder builder, RouteSettings settings, Offset offsetParam}) :
         offset = offsetParam,
         super(builder: builder, settings: settings);
@@ -42,6 +22,7 @@ class SlideTransitionRoute<T> extends MaterialPageRoute<T> {
       Animation<double> secondaryAnimation, Widget child) {
     if (settings.isInitialRoute) return child;
 
+    // Adapted from https://flutter.dev/docs/cookbook/animation/page-route-animation
     var begin = offset;
     var end = Offset.zero;
     var curve = Curves.ease;
@@ -53,8 +34,7 @@ class SlideTransitionRoute<T> extends MaterialPageRoute<T> {
       child: child,
     );
   }
-}
 
-int dragThreshold() {
-  return 10;
+  @override
+  Duration get transitionDuration => const Duration(seconds: 2);
 }
