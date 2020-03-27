2080759653d14e52d479afb36ec381faa5fb0cc7
vincentsastra <vincent.sastra@gmail.com>
2020-03-27 16:36:04 -0700

    wrapped goal page with provider

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -6,7 +6,6 @@ class AppState {
   List<AchievementGroup> achievementGroupList;
   List<TaskGroup> taskGroupList;
   TodoList todoList;
-  FinishList finishList;
 
   AppState() {
     // if data exist, load from persists, else
--- lib/screens/goal/screen_goal.dart
+++ lib/screens/goal/screen_goal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:treebeard/models/app_state.dart';
 import 'package:treebeard/screens/home/screen_home.dart';
 import '../routes.dart';
 
@@ -11,22 +13,30 @@ class GoalPage extends StatefulWidget {
 
 class _GoalPageState extends State<GoalPage> {
 
-  Widget content = Scaffold(
-    appBar: AppBar(),
-    body: Center(
-      child: Text('Goal Page'),
-    ),
-  );
+  final AppState appState = AppState();
+
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: content,
       onPanUpdate: (details) {
         if (details.delta.dx > dragThreshold()) {
           Navigator.pop(context);
         }
       },
+      child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider.value(value: appState.todoList),
+            ChangeNotifierProvider.value(value: appState.todoList.finishList)
+          ],
+          // TODO: FINISH HOW THE PAGE WILL LOOK IN THIS CHILD SECTION
+          child: Column(
+
+          )
+    ),
     );
   }
+
+
+
 }
